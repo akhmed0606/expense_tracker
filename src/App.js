b6cc2d2ab7ddf30d01ce746ac72b83578f39a11d
akhmed0606 <analgiev57@gmail.com>
2022-04-02 13:03:47 -0400

    add content in main and add dynamic styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import  Details  from './Components/Details/Details'
 import { Grid } from '@material-ui/core'
+import useStyles from './style'
 
 function App() {
+   const classes = useStyles()
   return (
     <div>
-    <Grid container spacing={0} alignItems='center' justifyContent='center' style={{height: '100vh'}}>
+    <Grid className={classes.grid} container spacing={0} alignItems='center' justifyContent='center' style={{height: '100vh'}}>
    <Grid item xs={12} sm={4}>
-      <Details />
+      <Details title='Income' />
    </Grid>
    <Grid item xs={12} sm={4}>
       Main
    </Grid>
    <Grid item xs={12} sm={4}>
-      <Details />
+      <Details title='Expense'/>
    </Grid>
     </Grid>
     </div>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Card, CardHeader,CardContent,Typography,Grid,Divider} from '@material-ui/core'
+import useStyles from './styles'
 
 const Main = () => {
+  const classes = useStyles()
   return (
-    <div>Main</div>
+   <Card className={classes.root}>
+     <CardHeader title='Expense Tracker' subheader='Speechly' />
+      <CardContent>
+        <Typography align='center' variant='h5'>Total Balanse</Typography>
+        <Typography variant='subtitle1'>
+          Say something
+          </Typography>
+      </CardContent>
+   </Card>
   )
 }
 
