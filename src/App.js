b02274e7f92a68016818581588087c67512e471f
akhmed0606 <analgiev57@gmail.com>
2022-04-01 20:41:31 -0400

    add Grid to main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import  Details  from './Components/Details/Details'
+import { Grid } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
+    <div>
+    <Grid container spacing={0} alignItems='center' justifyContent='center' style={{height: '100vh'}}>
+   <Grid item sm={12} xs={5}>
       <Details />
+   </Grid>
+    </Grid>
     </div>
   );
 }
