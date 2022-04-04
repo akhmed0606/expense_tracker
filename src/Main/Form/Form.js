ec2255506d9d43213c9ef31cce321d36fdea927a
akhmed0606 <analgiev57@gmail.com>
2022-04-04 16:24:54 -0400

    add grid from m-ui in form component

--- src/Main/Form/Form.js
+++ src/Main/Form/Form.js
@@ -1,8 +1,16 @@
 import { TextField, Typography,Grid,FormControl,Button,InputLabel,MenuItem,Select } from "@material-ui/core"
+import { v4 as uuidv4 } from 'uuid';
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <Grid container spacing={2}>
+    <Snackbar />
+    <Grid item xs={12}>
+      <Typography align="center" variant="subtitle2" gutterBottom>
+       {/* {isSpeaking ? <BigTranscript /> : 'Start adding transactions'}  */}
+      </Typography>
+    </Grid>
+    </Grid>
   )
 }
 
