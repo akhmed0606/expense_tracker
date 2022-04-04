b014267e89f5e9b135c5ecf43a5a6480064fd32a
akhmed0606 <analgiev57@gmail.com>
2022-04-04 16:27:50 -0400

    declare state variables for form data

--- src/Main/Form/Form.js
+++ src/Main/Form/Form.js
@@ -1,7 +1,12 @@
 import { TextField, Typography,Grid,FormControl,Button,InputLabel,MenuItem,Select } from "@material-ui/core"
+import { useState,useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 const Form = () => {
+  const classes = useStyles();
+  const [formData, setFormData] = useState()
+  const [open, setOpen] = useState(false);
+  
   return (
     <Grid container spacing={2}>
     <Snackbar />
