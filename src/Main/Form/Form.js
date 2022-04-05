f9ce0bb3acbdb25fd076a49814045fdef0f8f552
akhmed0606 <analgiev57@gmail.com>
2022-04-05 13:46:24 -0400

    create form control and input labels

--- src/Main/Form/Form.js
+++ src/Main/Form/Form.js
@@ -1,6 +1,7 @@
 import { TextField, Typography,Grid,FormControl,Button,InputLabel,MenuItem,Select } from "@material-ui/core"
 import { useState,useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import useStyles from './styles'
 
 const Form = () => {
   const classes = useStyles();
@@ -9,12 +10,38 @@ const Form = () => {
   
   return (
     <Grid container spacing={2}>
-    <Snackbar />
+    
     <Grid item xs={12}>
       <Typography align="center" variant="subtitle2" gutterBottom>
        {/* {isSpeaking ? <BigTranscript /> : 'Start adding transactions'}  */}
       </Typography>
     </Grid>
+      <Grid item xs={6}>
+       <FormControl fullWidth>
+         <InputLabel>Type</InputLabel>
+         <Select>
+           <MenuItem value='Income'>Income</MenuItem>
+           <MenuItem value='Expense'>Expense</MenuItem>
+         </Select>
+       </FormControl>
+       </Grid>
+       <Grid item xs={6}>
+          <FormControl>
+            <InputLabel>Category</InputLabel>
+            <Select>
+              {/* map values here */}
+              <MenuItem value='Income'>Business</MenuItem>
+           <MenuItem value='Expense'>Salary</MenuItem>
+            </Select>
+          </FormControl>
+       </Grid>
+     <Grid item xs={6}>
+     <TextField fullWidth type='number' label='Amount'/>
+     </Grid>
+     <Grid item xs={6}>
+     <TextField fullWidth type='date' label=''/>
+     </Grid>
+     <Button className={classes.button} variant='outlined' color='primary' fullWidth>Add</Button>
     </Grid>
   )
 }
